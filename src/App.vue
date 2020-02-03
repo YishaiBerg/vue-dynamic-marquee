82bdcb9540ad53c467b129aedfc6814b1a7111dd
YishaiBerg <ymb123@gmail.com>
2020-02-04 01:53:37 +0200

    translate with requestAnimation

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <template>
   <div id="app">
     <div class="outer-wrapper">
-    <dynamic-marquee>
-     <div>
-       Hello world
-     </div>
+    <dynamic-marquee :pps="10">
+      <ul>
+        <li v-for="i in liNum" :key="i">
+          Item {{i}}
+        </li>
+      </ul>
     </dynamic-marquee>
     </div>
   </div>
@@ -18,7 +20,15 @@ export default Vue.extend({
   name: 'app',
   components: {
     DynamicMarquee,
-  }
+  },
+  data() {
+    return {
+      liNum: 5
+    }
+  },
+  created () {
+    // setInterval(() => this.liNum++, 2000);
+  },
   });
 </script>
 <style scoped>
@@ -27,5 +37,11 @@ export default Vue.extend({
   height: 200px;
   width:  200px;
   border: 2px solid red;
+  position: absolute;
+  top: 40vh;
+}
+ul {
+  margin-block-start: 0;
+  margin-block-start: 0;
 }
 </style>
--- src/components/DynamicMarquee.vue
+++ src/components/DynamicMarquee.vue
@@ -1,8 +1,13 @@
 <template>
-  <div class="wrapper" :style="cssVars">
+  <div class="wrapper" :style="cssVars" ref="wrapper">
     <div class="marquee" ref="marquee">
       <slot></slot>
     </div>
+    <!-- <div class="marquee2" ref="marquee2">
+      <li v-for="i in 5" :key="i">
+          Item {{i}}
+        </li>
+    </div> -->
   </div>
 </template>
 
@@ -16,26 +21,61 @@ export default Vue.extend({
     pps: {
       type: Number,
       default: 20
-    },
+    }
   },
   data() {
     return {
+      wrapperHeight: 0,
       marqueeHeight: 0
     };
   },
   computed: {
     cssVars(): object {
       return {
+        "--wrapper-height": this.wrapperHeight + "px",
         "--marquee-height": this.marqueeHeight + "px",
         "--minus-marquee-height": -this.marqueeHeight + "px",
         "--marquee-speed": this.marqueeHeight / this.pps + "s"
       };
+    },
+    height() {
+      const a = this.$refs.marquee as Element;
+      return a.getBoundingClientRect().height;
     }
   },
   mounted() {
     this.$nextTick(() => {
+      const wrapper = this.$refs.wrapper as HTMLElement;
+      this.wrapperHeight = wrapper.getBoundingClientRect().height;
       const marquee = this.$refs.marquee as HTMLElement;
-      this.marqueeHeight = marquee.offsetHeight;
+      this.marqueeHeight = marquee.getBoundingClientRect().height;
+      // if(ResizeObserver) {
+      //   const resizeObserver = new ResizeObserver((enteries: any) => {
+      //     console.log(enteries)
+      //   })
+      // }
+
+      let lastTime = NaN;
+      let currentPosition = this.wrapperHeight;
+      let progress = 0;
+      const translateMarquee = (currentTime: number): void => {
+       if (lastTime) {
+         const elapsed = currentTime - lastTime;
+         const ratio = 1000 / elapsed;
+         const currentProgress = this.pps / ratio;
+         progress -= currentProgress;
+         marquee.style.transform = `translateY(${progress}px)`
+         console.log(currentProgress)
+       }
+        lastTime = currentTime;
+        if(progress > -(this.wrapperHeight + this.marqueeHeight)) {
+          requestAnimationFrame(translateMarquee)
+        }
+      };
+      requestAnimationFrame(translateMarquee);
+      //* pseudo code: if (marquee not higher than wrapper) {
+              // * translate 
+      //* }
     });
   }
 });
@@ -49,22 +89,44 @@ export default Vue.extend({
   position: relative;
 }
 
-@keyframes marquee {
+/* @keyframes marquee {
   0% {
-    top: var(--marquee-height);
+    top: var(--wrapper-height);
   }
   100% {
     top: var(--minus-marquee-height);
   }
 }
 
+@keyframes marquee2 {
+  0% {
+    top: calc(var(--wrapper-height) * 2 + 10px);
+  }
+  100% {
+    top: var(--minus-marquee-height);
+  }
+} */
+
 .marquee {
-  top: var(--marquee-height);
+  top: var(--wrapper-height);
   position: relative;
   box-sizing: border-box;
   animation: marquee var(--marquee-speed) linear infinite;
 }
 
+.marquee {
+  top: var(--wrapper-height);
+  position: relative;
+  box-sizing: border-box;
+  animation: marquee var(--marquee-speed) linear infinite;
+}
+
+.marquee2 {
+  top: calc(var(--wrapper-height) * 2 + 10px);
+  position: relative;
+  box-sizing: border-box;
+  animation: marquee 5s linear infinite;
+}
 .marquee:hover {
   animation-play-state: paused;
 }
