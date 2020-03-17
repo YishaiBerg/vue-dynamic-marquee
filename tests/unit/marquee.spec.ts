eb3cab52f29de61f1ce3a045ed493e77484ca92d
YishaiBerg <ymb123@gmail.com>
2020-03-17 16:03:18 +0200

    test repeatMargin after animation

--- tests/unit/marquee.spec.ts
+++ tests/unit/marquee.spec.ts
@@ -9,30 +9,28 @@ import Vue from 'vue';
 // replaceRaf();
 let rafStub: any;
 let spyRaf: any;
-// beforeEach(() => {
-//     rafStub = createStub();
-//     spyRaf = jest.spyOn(window, 'requestAnimationFrame').mockImplementation(rafStub.add);
-// });
-
-// afterEach(() => {
-//     jest.clearAllMocks();
-// });
-
-describe('DynamicMarquee', () => {
+beforeAll(() => {
     rafStub = createStub();
     spyRaf = jest.spyOn(window, 'requestAnimationFrame').mockImplementation(rafStub.add);
+});
+
+afterAll(() => {
+    jest.clearAllMocks();
+});
+
+describe('DynamicMarquee', () => {
 
     const wrapper = mount(DynamicMarquee, {
         propsData: {
-            repeatMargin: 10,
+            repeatMargin: 1,
             direction: 'row',
         },
         data() {
             return {
                 testData: {
                     inTest: true,
-                    warpperDimension: 120,
-                    marqueeDimension: 20,
+                    wrapperDimension: 12,
+                    marqueeDimension: 2,
                     wrapperDirection: 'ltr',
                 },
             };
@@ -41,7 +39,6 @@ describe('DynamicMarquee', () => {
 
     // TODO: test with repeat = false
     it('should duplicate slots as necessary', () => {
-        console.log(wrapper.vm.repeatMargin);
         expect(wrapper.vm.$data.repeatNum).toBe(5);
     });
 
@@ -54,15 +51,28 @@ describe('DynamicMarquee', () => {
     
     
     it('should call raf', () => {
-        rafStub.step(250);
+        rafStub.step(100);
         console.log(wrapper.html())
         expect(spyRaf).toBeCalled();
-        expect(wrapper.vm.animatedElements.length).toBeGreaterThanOrEqual(4);
     });
     
     it('should reanimate elements', () => {
+        expect(wrapper.vm.animatedElements.length).toBeGreaterThanOrEqual(4);
+    });
+    
+    it('should keep repeatMargin distance between animated elements', () => {
+        const progressArr = wrapper.vm.animatedElements.map((el) => Math.floor(el.progress));
+        let lastVal = progressArr[0];
+        let wrongMargin = false;
+        const difference = wrapper.vm.repeatMargin + wrapper.vm.marqueeDimension;
+        for (let i = 1; i < progressArr.length; i++) {
+            if ((lastVal - difference) !== progressArr[i]) {
+                wrongMargin = true;
+            }
+            lastVal = progressArr[i];
+        }
+        expect(wrongMargin).toBe(false);
         wrapper.destroy();
-        jest.clearAllMocks();
-    })
+    });
 
 });
