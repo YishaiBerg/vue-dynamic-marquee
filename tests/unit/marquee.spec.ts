9747faafd485bb6c07bf709b7adfb24f295caa09
YishaiBerg <ymb123@gmail.com>
2020-03-17 03:20:36 +0200

    begin raf tests

--- tests/unit/marquee.spec.ts
+++ tests/unit/marquee.spec.ts
@@ -3,12 +3,25 @@
 import { mount } from '@vue/test-utils';
 import DynamicMarquee from '../../src/components/DynamicMarquee';
 // import '../../declarations.d.ts';
-import { replaceRaf } from 'raf-stub';
+import createStub from 'raf-stub';
 import Vue from 'vue';
 
-replaceRaf();
+// replaceRaf();
+let rafStub: any;
+let spyRaf: any;
+// beforeEach(() => {
+//     rafStub = createStub();
+//     spyRaf = jest.spyOn(window, 'requestAnimationFrame').mockImplementation(rafStub.add);
+// });
+
+// afterEach(() => {
+//     jest.clearAllMocks();
+// });
 
 describe('DynamicMarquee', () => {
+    rafStub = createStub();
+    spyRaf = jest.spyOn(window, 'requestAnimationFrame').mockImplementation(rafStub.add);
+
     const wrapper = mount(DynamicMarquee, {
         propsData: {
             repeatMargin: 10,
@@ -26,14 +39,30 @@ describe('DynamicMarquee', () => {
         },
     });
 
+    // TODO: test with repeat = false
     it('should duplicate slots as necessary', () => {
+        console.log(wrapper.vm.repeatMargin);
         expect(wrapper.vm.$data.repeatNum).toBe(5);
     });
 
+    // TODO: check all combinations
     it('should correctly set marquee initial position', () => {
         const marqueeElement = wrapper.find({ name: 'dynamic-marquee-element' });
         const elStyle = marqueeElement.attributes('style');
         expect(elStyle).toEqual(expect.stringContaining('right: 100%'));
-        wrapper.destroy();
     });
+    
+    
+    it('should call raf', () => {
+        rafStub.step(250);
+        console.log(wrapper.html())
+        expect(spyRaf).toBeCalled();
+        expect(wrapper.vm.animatedElements.length).toBeGreaterThanOrEqual(4);
+    });
+    
+    it('should reanimate elements', () => {
+        wrapper.destroy();
+        jest.clearAllMocks();
+    })
+
 });
